3768f077df55b3ffc58ee39c5d36188d7e2e3976
Henrik Huhtaluoma <henrik.huhtaluoma@gmail.com>
2016-09-02 11:58:09 +0300

    Base functions ready v0.1

--- thebutler/thebutlermaster/thebutlerslave/views.py
+++ thebutler/thebutlermaster/thebutlerslave/views.py
@@ -1,17 +1,40 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os, sys
+import json, requests, random, re
 from pprint import pprint
-import json
-import requests
 from django.views import generic
 from django.http.response import HttpResponse
 
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 
-def post_facebook_message(fbid, recevied_message):           
-    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAHNADSZAI2gBAIrlsOJEnnC4Nl2aqBtoZA15MgIys06pLgsngJpQ2yg3KFNNhceBnNdZCJpjj92KQxKy8kHJZCoZAJa59gYWcBylimZCcIiNoi67OkI1CZAIHdblWj6UkdhYv8ZAWCWma7gl699D9PY7Bli1VRfoqwEXlH16jwSMgZDZD' 
-    response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":recevied_message}})
-    status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
-    pprint(status.json())           
+vocabulary = {
+         'ruoka': ["""Tässä kerron kuvailevasti yrityksen ruokatarjonnasta, ja siitä kuinka herkullista se on.""",
+                    """Testiravintolamme tarjoaa herkullista brunssia, lounasta ja illallista. Kokeile kuuluisaa kolmen annoksen menuamme!"""],
+         'taide': ["""Järjestämme tiloissamma taidenäyttelyitä ja erilaisia gallerioita, joissa paikalliset lahjakkuudet voivat esitellä töitään.""",
+                    """Taiteeseen liittyvissä asiossa ota yhteyttä esimerkki@esimerkki.fi"""],
+         'yritys': ["""Yrityksemme on perustettu vuonna 2012.""",
+                    """Työllistämme kuusi työntekijää ja ruokaamme on kehuttu muunmuassa Helsingin Sanomissa."""],
+	
+	 'toiminta': ["""Olemme mukana vaikuttamassa erilaisilla festivaaleilla ja näyttelyissä.""",
+		      """Toimintamme on Kallion trendikkäintä!"""]
+
+         }
+
+def post_facebook_message(fbid, recevied_message):
+ tokens = re.sub(r"[^a-zA-Z0-9\s]",' ',recevied_message).lower().split()
+ vocabulary_text = ''
+ for token in tokens:
+     if token in vocabulary:
+         vocabulary_text = random.choice(vocabulary[token])
+         break
+ if not vocabulary_text:
+     vocabulary_text = "Anteeksi herra, en ymmärtänyt kysymystänne. Osaamisalani ovat: 'ruoka','taide','yritys' ja 'toiminta'"           
+ post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAHNADSZAI2gBAIrlsOJEnnC4Nl2aqBtoZA15MgIys06pLgsngJpQ2yg3KFNNhceBnNdZCJpjj92KQxKy8kHJZCoZAJa59gYWcBylimZCcIiNoi67OkI1CZAIHdblWj6UkdhYv8ZAWCWma7gl699D9PY7Bli1VRfoqwEXlH16jwSMgZDZD' 
+ response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":vocabulary_text}})
+ status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
+ pprint(status.json())           
   
 
 
